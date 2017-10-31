cb0a63ad8f1af06d3b6b837c0b2dc104d8675c32
yangsa <18437976116@163.com>
2017-10-31 14:54:17 +0800

    修改了source.html,left.html,operat_table.js

--- public/admin_static/js/operat_table.js
+++ public/admin_static/js/operat_table.js
@@ -1,183 +1,161 @@
-$(document).ready(function(){
-var trs=$("#tbody_center tr");
-//	console.log($(".check"),8888)
-var checks=$(".check");
-//	console.log(trs.children("td").find(".check"),8888)
-checks.live("click",function(event){
-		
-    event.stopPropagation();
-    $(".modal_box").show();
-        
-    var userId=$(this).parent().parent().children("td:nth-child(1)").html();
-    $.post("/abab.php/user/getUserInfo",{"uid":userId},function(obj){
-            if(obj.state_code == 200){
-                $(".userId").html(userId);
-                $(".phone").html(obj.data.mobile);
-                $(".nickname").html(obj.data.nickname);
-                $(".birthday").html(obj.data.birthday);
-                $(".hobby").html(obj.data.hobby);
-                $(".normal").html(obj.data.status);//状态
-                $(".address").html(obj.data.address);
-                $(".start").html(obj.data.reg_time);//注册时间
-                $(".finsh").html(obj.data.last_time);//最后登录时间
-                $(".province").html(obj.data.province);//所在省
-                $(".country").html(obj.data.city);
-                $(".area").html(obj.data.district);//区
-                $(".sign").html(obj.data.signature);
-            }else{
-                alert(obj.msg)
-            }
-    })
-})
-        
-//弹出关闭按钮
-$(".cancel").on("click",function(){
-        $(".modal_box").hide();
-})
-	
-	
+$(document).ready(function() {
+	var trs = $("#tbody_center tr");
+	//	console.log($(".check"),8888)
+	var checks = $(".check");
+	//	console.log(trs.children("td").find(".check"),8888)
+	checks.live("click", function(event) {
 
+		event.stopPropagation();
+		$(".modal_box").show();
 
-//取消(是否确认取消)
-$(".btn-warning").on("click",function(){
-	return confirm("是否确认取消");
-})
+		var userId = $(this).parent().parent().children("td:nth-child(1)").html();
+		$.post("/abab.php/user/getUserInfo", { "uid": userId }, function(obj) {
+			if(obj.state_code == 200) {
+				$(".userId").html(userId);
+				$(".phone").html(obj.data.mobile);
+				$(".nickname").html(obj.data.nickname);
+				$(".birthday").html(obj.data.birthday);
+				$(".hobby").html(obj.data.hobby);
+				$(".normal").html(obj.data.status); //状态
+				$(".address").html(obj.data.address);
+				$(".start").html(obj.data.reg_time); //注册时间
+				$(".finsh").html(obj.data.last_time); //最后登录时间
+				$(".province").html(obj.data.province); //所在省
+				$(".country").html(obj.data.city);
+				$(".area").html(obj.data.district); //区
+				$(".sign").html(obj.data.signature);
+			} else {
+				alert(obj.msg)
+			}
+		})
+	})
 
-function deleteData(url,option,parent){
-	var trueOrfalse=confirm("是否确认删除");
-    var _this = $(this);
-    if(!trueOrfalse){
-        	return;
-    }
-    else{
-        
-    	
-    	$.post(url,option,function(data){ 
-    		
-                if(data.state_code== 200){
-                	parent.hide();
-                }else{
-                    alert(data.msg)
-                }
-        })
-    }
-    
-   
-}
-//活动页面删除
-$(".activity_delete").live("click",function(){
-   	deleteData("/abab.php/activity/delActivity",
-   	{"aid":parseInt($(this).parent().parent().children("td:nth-child(1)").text())},
-   	$(this).parent().parent())
-})
-//活动分类删除
-$(".classfy_delete").live("click",function(){
-   	deleteData("/abab.php/activity/delType",
-   	{"id":parseInt($(this).parent().parent().parent().find("td").eq(0).html())}
-   	,$(this).parent().parent().parent())
-})
-//评论提问删除
-$(".ask_delete").live("click",function(){
-   	deleteData("/abab.php/activity/delAnyReply",
-   	{"id":parseInt($(this).parent().parent().parent().find("td").eq(0).html())},
-   	$(this).parent().parent().parent())
-   	
-})
-//扩展删除
-$(".ext_delete").live("click",function(){
-   	deleteData("/abab.php/activity/del_extension",
-   	{"id":parseInt($(this).parent().siblings(".td_id").html())},
-   	$(this).parent().parent())
-   	
-})
-//评论删除
-$(".com_delete").live("click",function(){
-   	deleteData("/abab.php/activity/delAnyReply",
-   	{"id":parseInt($(this).parent().parent().find("td").eq(0).html())},
-   	$(this).parent().parent())
-   	
-})
-//活动规格
-$(".spe_delete").live("click",function(){
-   	deleteData("/abab.php/activity/delAnySpe",
-   	{"id":parseInt($(this).parent().siblings(".td_id").html())},
-   	$(this).parent().parent())
-   	
-})
+	//弹出关闭按钮
+	$(".cancel").on("click", function() {
+		$(".modal_box").hide();
+	})
 
-//成单删除
-$(".deal_delete").live("click",function(){
-   	deleteData("/abab.php/user/del_deal",
-   	{"id":parseInt($(this).parent().siblings(".td_id").html())},
-   	$(this).parent().parent())
-   	
-})
+	//取消(是否确认取消)
+	$(".btn-warning").on("click", function() {
+		return confirm("是否确认取消");
+	})
 
+	function deleteData(url, option, parent) {
+		var trueOrfalse = confirm("是否确认删除");
+		var _this = $(this);
+		if(!trueOrfalse) {
+			return;
+		} else {
 
-$(".select_activity").on("change",function(){
-	var str = '';
-	var Activityid=$(this).val();
-	$.post("/abab.php/activity/getActivityTime",{aid:Activityid},
-    function(obj){
-    	if(obj.state_code==-1){
-    		$(".warn").show();
-    		$(".warn").html(obj.msg);
-    		$("#s2id_select_time span").html('');
-    		$("#select_time").empty();
+			$.post(url, option, function(data) {
 
+				if(data.state_code == 200) {
+					parent.hide();
+				} else {
+					alert(data.msg)
+				}
+			})
+		}
 
-    	}
-        else  if(obj.state_code == 200){
-        	
-        	
-        		
-	        	for(var i=0;i<obj.data.length;i++){
-	        		var t_id=obj.data[i].t_id;
-	        		str += "<option value="+t_id+">"+obj.data[i].t_content+"</option>"; 
-	        	   
-	        	   
-	        	}
-	        	
-        	
-        		$("#select_time").html(str);
-        		$(".warn").hide();
-        	
-        	
-        	
-        }
-    }, "json");
-	
-})
+	}
+	//活动页面删除
+	$(".activity_delete").live("click", function() {
+		deleteData("/abab.php/activity/delActivity", { "aid": parseInt($(this).parent().parent().children("td:nth-child(1)").text()) },
+			$(this).parent().parent())
+	})
+	//活动分类删除
+	$(".classfy_delete").live("click", function() {
+		deleteData("/abab.php/activity/delType", { "id": parseInt($(this).parent().parent().parent().find("td").eq(0).html()) }, $(this).parent().parent().parent())
+	})
+	//评论提问删除
+	$(".ask_delete").live("click", function() {
+		deleteData("/abab.php/activity/delAnyReply", { "id": parseInt($(this).parent().parent().parent().find("td").eq(0).html()) },
+			$(this).parent().parent().parent())
+
+	})
+	//扩展删除
+	$(".ext_delete").live("click", function() {
+		deleteData("/abab.php/activity/del_extension", { "id": parseInt($(this).parent().siblings(".td_id").html()) },
+			$(this).parent().parent())
+
+	})
+	//评论删除
+	$(".com_delete").live("click", function() {
+		deleteData("/abab.php/activity/delAnyReply", { "id": parseInt($(this).parent().parent().find("td").eq(0).html()) },
+			$(this).parent().parent())
+
+	})
+	//活动规格
+	$(".spe_delete").live("click", function() {
+		deleteData("/abab.php/activity/delAnySpe", { "id": parseInt($(this).parent().siblings(".td_id").html()) },
+			$(this).parent().parent())
+
+	})
+
+	//成单删除
+	$(".deal_delete").live("click", function() {
+		deleteData("/abab.php/user/del_deal", { "id": parseInt($(this).parent().siblings(".td_id").html()) },
+			$(this).parent().parent())
+
+	})
+
+	$(".select_activity").on("change", function() {
+		var str = '';
+		var Activityid = $(this).val();
+		$.post("/abab.php/activity/getActivityTime", { aid: Activityid },
+			function(obj) {
+				if(obj.state_code == -1) {
+					$(".warn").show();
+					$(".warn").html(obj.msg);
+					$("#s2id_select_time span").html('');
+					$("#select_time").empty();
+
+				} else if(obj.state_code == 200) {
+
+					for(var i = 0; i < obj.data.length; i++) {
+						var t_id = obj.data[i].t_id;
+						str += "<option value=" + t_id + ">" + obj.data[i].t_content + "</option>";
+
+					}
+
+					$("#s2id_select_time span").html('请选择');
+					$("#select_time").html(str);
+
+					$(".warn").hide();
+
+				}
+			}, "json");
+
+	})
 
 })
 
 //省改变事件
-function seachcitys(province){
-    $.get("/home/user/getRegion",{id:province},
-    function(obj){
-        if(obj.state_code == 200){
-            $("#seachcity").html(obj.data);
-        }
-    }, "json");
+function seachcitys(province) {
+	$.get("/home/user/getRegion", { id: province },
+		function(obj) {
+			if(obj.state_code == 200) {
+				$("#seachcity").html(obj.data);
+			}
+		}, "json");
 }
 //市改变事件
-function seachdistricts(city){
-    $.get("/home/user/getRegion",{id:city},
-    function(obj){
-        if(obj.state_code == 200){
-            $("#seachdistrict").html(obj.data);
-        }
-    }, "json");
+function seachdistricts(city) {
+	$.get("/home/user/getRegion", { id: city },
+		function(obj) {
+			if(obj.state_code == 200) {
+				$("#seachdistrict").html(obj.data);
+			}
+		}, "json");
 }
 //活动类型改变事件
-function changeActivityType(tid){
-    $.get("/abab.php/activity/getSonType",{id:tid},
-    function(obj){
-        if(obj.state_code == 200){
-            $("#sonType").html(obj.data);
-        }
-    }, "json");
+function changeActivityType(tid) {
+	$.get("/abab.php/activity/getSonType", { id: tid },
+		function(obj) {
+			if(obj.state_code == 200) {
+				$("#sonType").html(obj.data);
+			}
+		}, "json");
 }
-
 
 //是否确认删除按钮
