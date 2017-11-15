af71aeb74339418fc0d2cd4b1d82d7bf327aa482
sqz2244963 <63526108@qq.com>
2017-11-15 16:25:25 +0800

    导入增加孩子信息

--- application/admin/controller/Activity.php
+++ application/admin/controller/Activity.php
@@ -671,35 +671,46 @@ class Activity extends Base
         $k = 0;
         for($j=2;$j<=$highestRow;$j++)
         {
-            $a = $objPHPExcel->getActiveSheet()->getCell("A".$j)->getValue();//获取A列的值，用户名
-            $b = $objPHPExcel->getActiveSheet()->getCell("B".$j)->getValue();//获取B列的值，手机号
-            $c = $objPHPExcel->getActiveSheet()->getCell("C".$j)->getValue();//获取C列的值，下单时间
-            $d = $objPHPExcel->getActiveSheet()->getCell("D".$j)->getValue();//获取D列的值，支付时间
-            $e = $objPHPExcel->getActiveSheet()->getCell("E".$j)->getValue();//获取E列的值，付款金额
-            $f = $objPHPExcel->getActiveSheet()->getCell("F".$j)->getValue();//获取列的值，来源
+            $A = $objPHPExcel->getActiveSheet()->getCell("A".$j)->getValue();//获取A列的值，用户名
+            $B = $objPHPExcel->getActiveSheet()->getCell("B".$j)->getValue();//获取B列的值，手机号
+            $C = $objPHPExcel->getActiveSheet()->getCell("C".$j)->getValue();//获取C列的值，下单时间
+            $D = $objPHPExcel->getActiveSheet()->getCell("D".$j)->getValue();//获取D列的值，支付时间
+            $E = $objPHPExcel->getActiveSheet()->getCell("E".$j)->getValue();//获取E列的值，付款金额
+            $F = $objPHPExcel->getActiveSheet()->getCell("F".$j)->getValue();//获取F列的值，来源
+            $G = $objPHPExcel->getActiveSheet()->getCell("G".$j)->getValue();//获取G列的值，孩子姓名
+            $H = $objPHPExcel->getActiveSheet()->getCell("H".$j)->getValue();//获取H列的值，孩子性别
+            $I = $objPHPExcel->getActiveSheet()->getCell("I".$j)->getValue();//获取I列的值，孩子生日
+            $J = $objPHPExcel->getActiveSheet()->getCell("J".$j)->getValue();//获取J列的值，孩子可玩耍时间
+            $K = $objPHPExcel->getActiveSheet()->getCell("K".$j)->getValue();//获取K列的值，孩子学校
 
             //手机号，来源为必填项，任何一个为空就不记录
-            if(empty($b) || empty($f)){
+            if(empty($B) || empty($F)){
                 continue;
             }
             //下单时间，支付时间为空时默认导入时间，付款金额为空是默认0
             if(empty($c)){
                 $c = date('Y-m-d H:i:s');
             }
-            if(empty($d)){
-                $d = date('Y-m-d H:i:s');
+            if(empty($D)){
+                $D = date('Y-m-d H:i:s');
             }
-            if(empty($e)){
-                $e = 0;
+            if(empty($E)){
+                $E = 0;
             }
-            $excel_data[$k]['name'] = $a;
-            $excel_data[$k]['mobile'] = $b;
-            $excel_data[$k]['addtime'] = strtotime($c);
-            $excel_data[$k]['pay_time'] = strtotime($d);
-            $excel_data[$k]['order_price'] = $e;
-            $excel_data[$k]['source'] = $f;
+            $excel_data[$k]['name'] = $A;
+            $excel_data[$k]['mobile'] = $B;
+            $excel_data[$k]['addtime'] = strtotime($C);
+            $excel_data[$k]['pay_time'] = strtotime($D);
+            $excel_data[$k]['order_price'] = $E;
+            $excel_data[$k]['source'] = $F;
+            $excel_data[$k]['child_name'] = $G?$G:'';
+            $excel_data[$k]['child_gender'] = $H?$H:'';
+            $excel_data[$k]['child_birthday'] = $I?$I:'';
+            $excel_data[$k]['child_play_time'] = $J?$J:'';
+            $excel_data[$k]['child_school'] = $K?$K:'';
             $k++;
         }
+
         /*//获取时间段信息
         if($tid != 0){
             $timeInfo = model('ActivityTime')->getAnyTime($tid);
@@ -738,6 +749,14 @@ class Activity extends Base
             $add_data[$i]['addtime'] = $excel_data[$i]['addtime'];
             $add_data[$i]['source'] = $source;
             $add_data[$i]['t_id'] = $tid;
+
+            //孩子信息
+            $child_data[$i]['mobile'] = $excel_data[$i]['mobile'];
+            $child_data[$i]['child_name'] = $excel_data[$i]['child_name'];
+            $child_data[$i]['child_gender'] = $excel_data[$i]['child_gender'];
+            $child_data[$i]['child_birthday'] = $excel_data[$i]['child_birthday'];
+            $child_data[$i]['child_play_time'] = $excel_data[$i]['child_play_time'];
+            $child_data[$i]['child_school'] = $excel_data[$i]['child_school'];
         }
         //去除重复,用于添加到客户表
         $result = array();
@@ -752,9 +771,11 @@ class Activity extends Base
                 }
                 if(!$set){
                     $result[] = $val;
+                    $result_child[] = $child_data[$key];
                 }
             }
         }
+
         //导入进来的名单如果没有在客户表里，就添加到表中
         $i = 0;
         foreach($result as $k=>$v){
@@ -771,6 +792,34 @@ class Activity extends Base
         if(isset($add_user)){
             db('user')->insertAll($add_user);
         }
+
+        //添加孩子
+        $i=0;
+        foreach($result_child as $k=>$v){
+            if(!empty($v['child_name'])){
+                $uid = db('user')->where('mobile',$v['mobile'])->value('uid');
+                $add_child_data[$i]['uid'] = $uid;
+                $add_child_data[$i]['name'] = $v['child_name'];
+                if($v['child_gender'] != ''){
+                    if($v['child_gender'] == '男'){
+                        $add_child_data[$i]['gender'] = 1;
+                    }else{
+                        $add_child_data[$i]['gender'] = 2;
+                    }
+                }else{
+                    $add_child_data[$i]['gender'] = 0;
+                }
+                $add_child_data[$i]['birthday'] = $v['child_birthday'];
+                $add_child_data[$i]['school'] = $v['child_school'];
+                $add_child_data[$i]['play_time'] = $v['child_play_time'];
+                $add_child_data[$i]['time'] = date('Y-m-d H:i:s');
+                $i++;
+            }
+        }
+        if(isset($add_child_data)){
+            db('user_child')->insertAll($add_child_data);
+        }
+
         /*$k_num = count($add_data);
         //减库存
         $Activity->where('aid',$aid)->setDec('a_num',$k_num);
