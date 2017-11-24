4e4a83ec0080bbeb1f49874c97242b672796c40c
sqz2244963 <63526108@qq.com>
2017-11-24 18:12:50 +0800

    修改输入金额验证

--- application/mobile/controller/User.php
+++ application/mobile/controller/User.php
@@ -585,6 +585,7 @@ class User extends Base
     public function enter_recharge(){
         //金额
         $money = input('post.money');
+        $money = sprintf("%.2f",substr(sprintf("%.3f", $money), 0, -1));
         //检查金额
         if(floatval($money) == 0){
             $this->error('金额输入不正确');
