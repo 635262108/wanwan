1e803bec5eb874b261b173669ee2c8f45fc565d4
sqz2244963 <63526108@qq.com>
2017-12-28 15:29:51 +0800

    新增增加订单接口，用户查询接口

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -30,4 +30,74 @@ class Order extends Controller
             return_info(-1,'fail');
         }
     }
+
+    //添加订单
+    public function addOrder(){
+        $data = input('post.');
+        $validate = validate('Order');
+        $check = $validate->scene('addOrder')->check($data);
+        if (!$check) {
+            return_info(-1,$validate->getError());
+        }
+        //检查手机号是否已注册，未注册需要注册
+        $userInfo = model('User')->getMobileUserInfo($data['mobile']);
+        if(empty($userInfo)){
+            $user_data = [
+                'mobile' => $data['mobile'],
+                'nickname' => $data['name'],
+                'reg_time' => time(),
+                'source' => $data['source'],
+            ];
+            $uid = model('User')->insertGetId($user_data);
+        }else{
+            $uid = $userInfo->uid;
+        }
+        $activityInfo = model('Activity')->get($data['aid']);
+        if(empty($activityInfo)){
+            return_info(-1,'活动不存在');
+        }
+        $data['order_price'] = $data['adult_num']*$activityInfo['a_adult_price'] + $data['child_num']*$activityInfo['a_child_price'];
+        //签到时间存在时记录签到时间,订单状态为已签到
+        if($data['sign_time'] == 1){
+            $data['sign_time'] = time();
+            $data['order_status'] = 4;
+        }else{
+            $data['order_status'] = 3;
+        }
+        //支付方式为余额支付时，记录支付信息
+        if($data['pay_way'] == 4){
+            //检查余额够不够
+            if($userInfo->balance < $data['order_price']){
+                return_info(-1,'余额不足');
+            }
+            $userInfo->balance = $userInfo->balance - $data['order_price'];
+            $saveInfo = $userInfo->save();
+
+            //记录订单明细
+            $detail_data = [
+                'uid' => $uid,
+                'type' => 3,
+                'money' => $data['order_price'],
+                'balance' => $userInfo->balance,
+                'addtime' => time(),
+                'remark' => '参加'.$activityInfo['a_title'],
+            ];
+
+            $addDetail = model('UserDetail')->insert($detail_data);
+
+            if(!$saveInfo || !$addDetail){
+                return_info(-1,'扣费失败');
+            }
+        }
+        $data['order_sn'] = getOrderSn($uid,$data['aid']);
+        $data['uid'] = $uid;
+        $data['pay_time'] = time();
+        $data['addtime'] = time();
+        $addOrder = model('ActivityOrder')->insert($data);
+        if($addOrder){
+            return_info(200,'success');
+        }else{
+            return_info(-1,'记录订单失败');
+        }
+    }
 }
---
+++ application/common/validate/Order.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/25 0025
+ * Time: 下午 5:51
+ */
+
+namespace app\common\validate;
+use think\Validate;
+
+class Order extends Validate
+{
+    protected $regex = [
+        'mobile' => '#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#'
+    ];
+
+    protected $rule = [
+        'mobile' => 'require|mobile',
+        'name'  =>  'require|max:25',
+        'aid' => 'require|integer',
+        't_id' => 'integer',
+        'adult_num' => 'require|integer',
+        'child_num' => 'require|integer',
+        'pay_way' => 'integer',
+        'source' => 'require|integer',
+        'sign_time' => 'require|integer',
+    ];
+
+    protected $message = [
+        'mobile.require' => '手机号不能为空',
+        'mobile.mobile' => '手机号格式不正确',
+        'name' => '姓名格式不正确',
+        'aid' => '活动格式不正确',
+        't_id' => '活动时间不正确',
+        'adult_num' => '大人数量不正确',
+        'child_num' => '小孩数量不正确',
+        'pay_way' => '支付方式格式不正确',
+        'source' => '来源格式不正确',
+        'sign_time' => '签到时间格式不正确',
+    ];
+
+    protected $scene = [
+        'addOrder' => 'mobile,name,aid,t_id,adult_num,child_num,pay_way,source,sign_time'
+    ];
+}
