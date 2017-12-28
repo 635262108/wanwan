a33d5a3d5f99caef12df2b326bd3e5ccf7ed71d5
sqz2244963 <63526108@qq.com>
2017-12-28 10:08:12 +0800

    签到概况增加签到早退功能

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -10,5 +10,24 @@ use think\Controller;
 
 class Order extends Controller
 {
+    //修改订单状态
+    public function saveOrderStatus(){
+        $data = input('post.');
+        $orderInfo = model('ActivityOrder')->get($data);
+        if(empty($orderInfo)){
+            return_info(-1,'订单号错误');
+        }
+        //修改状态为签到时，记录签到时间
+        if($data['order_status'] == 4){
+            $orderInfo->sign_time = time();
+        }
 
+        $orderInfo->order_status = $data['order_status'];
+        $res = $orderInfo->save();
+        if($res){
+            return_info(200,'success');
+        }else{
+            return_info(-1,'fail');
+        }
+    }
 }
