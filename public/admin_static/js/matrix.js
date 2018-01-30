c1faa19b4852fdb57ae0d6b7e42d16ac4a5dacd5
yangsa <18437976116@163.com>
2018-01-30 18:42:19 +0800

    修改iframe

--- public/admin_static/js/matrix.js
+++ public/admin_static/js/matrix.js
@@ -1,19 +1,10 @@
-
-$(document).ready(function(){
-
-	
-	
+   
+		
 	// === Sidebar navigation === //
 	
 	$("#index > a").click(function(e){
-//		var href=$(this).attr("href");
-//	    $("iframe").attr("src",href);
-//		sessionStorage.setItem("href_name",$(this).attr("href"));
-//		
-//		sessionStorage.removeItem("index");
-//		
-//		$(".submenu").removeClass("active")
-		
+//      localStorage.removeItem("Url");
+//		localStorage.removeItem("ulIndex");
 	})
 	
 	
@@ -23,38 +14,33 @@ $(document).ready(function(){
 //	侧边栏的点击事件
     
     var sideLis=$(".leftSide>ul").find(".submenu");
-    
     var lisContents=$(".listContent").children();
     sideLis.on("click",function(){
+    	
     	var index=$(this).index();
     	$(this).addClass("active").siblings().removeClass("active")
     	lisContents.eq(index-1).show().siblings().hide();
     	lisContents.eq(index-1).children("li:nth-child(1)").find("a").addClass("active");
     	lisContents.eq(index-1).children("li:nth-child(1)").siblings().find("a").removeClass("active");
       	var first_href=lisContents.eq(index-1).children("li:nth-child(1)").find("a").attr("href");
-      	$("iframe").attr({"width":"100%","height":"1190px","src":first_href});
-      	$(this).parents(".wholePage").siblings("#content").hide()
-      	
-    	
-         
-//  	sessionStorage.setItem("href_name",first_href);
-//  	sessionStorage.setItem("index",index);
-//  	window.location.href=first_href;
-})
+      	$("iframe").attr({"width":"100%","height":"1400px","src":first_href});
+      	$(this).parents(".wholePage").siblings("#content").hide();
+//    	 JSON.stringify(localStorage.setItem('Url',first_href));
+//    	 localStorage.setItem('ulIndex',index)
+    })
     
     
 //  点击重要的部分
     lisContents.each(function(index){
     	var lis=$(this).children("li");
-    	
     	lis.on("click",function(){
     	  var href=$(this).find("a").attr("href");
     	  $(this).find("a").addClass("active");
     	  $(this).siblings().find("a").removeClass("active");
     	  $("iframe").attr("src",href);
           $(this).parents(".wholePage").siblings("#content").hide();
-    	})
+//        JSON.stringify(localStorage.setItem('Url',href));
+        })
     })
 
 	
-});
--- public/admin_static/js/operat_table.js
+++ public/admin_static/js/operat_table.js
@@ -994,18 +994,16 @@ $(".amendSave").on("click",function(event){
 
 
 })
-//点击筛选条件（会员列表）
-
-//操作明细记录里面的选项
+//操作明细记录里面的选项(充值，扣费，付费)
 var particularslis=$(".particularsUl").children();
-
 particularslis.on("click",function(){
     var index=$(this).index();
     var typeNum=index+1;
     var uId=$(".hidden_val").val();
-    var str=''
-    $(this).addClass("active").siblings().removeClass("active");
-    $.post('/api/user/getAnyUserConsumption',{uid:uId,type:typeNum
+    var str='';
+    if($(this).hasClass("active")){
+    	$(this).removeClass("active");
+     $.post('/api/user/getAnyUserConsumption',{uid:uId,type:''
 		
 	},function(obj){
 		if(obj.state_code == 200){
@@ -1045,6 +1043,91 @@ particularslis.on("click",function(){
 			
 		}
 	})
+    }
+    else{
+    	$(this).addClass("active").siblings().removeClass("active");
+      	  $.post('/api/user/getAnyUserConsumption',{uid:uId,type:typeNum
+		
+	},function(obj){
+		if(obj.state_code == 200){
+			
+			for(var i=0;i<obj.data.length;i++){
+				
+				str+="<tr>"+"<td>"+obj.data[i].id+"</td>"
+				           +"<td>"+obj.data[i].money+"</td>"
+						   +"<td>"+obj.data[i].balance+"</td>"
+						   +"<td>"+obj.data[i].type+"</td>"
+						   +"<td>"+obj.data[i].remark+"</td>"
+						   +"<td>"+obj.data[i].addtime+"</td>"
+						   +"<td class='center'><i class='delete icon-pencil repeal'>&nbsp;&nbsp;撤销</i>	</td>"
+					  "</tr>"
+				
+				
+			}
+			
+			Table.fnClearTable();
+			Table.fnDestroy();
+			$(".detailTable").html(str);
+			$(".data-table").attr("style","width: 100%;")
+		    Table=$('.data-table').dataTable({
+			    "bDestroy":true,
+				"bJQueryUI": true,
+				"retrieve":true,
+				"sPaginationType": "full_numbers",
+				"sDom": '<""l>t<"F"fp>',
+				"bFilter":true,
+				"bLengthChange":true,
+				"aaSorting":[[0,"desc"]],
+				"bAutoWidth":true
+			});
+			
+
+			
+			
+		}
+	})
+    }
+    
+//  $.post('/api/user/getAnyUserConsumption',{uid:uId,type:typeNum
+//		
+//	},function(obj){
+//		if(obj.state_code == 200){
+//			
+//			for(var i=0;i<obj.data.length;i++){
+//				
+//				str+="<tr>"+"<td>"+obj.data[i].id+"</td>"
+//				           +"<td>"+obj.data[i].money+"</td>"
+//						   +"<td>"+obj.data[i].balance+"</td>"
+//						   +"<td>"+obj.data[i].type+"</td>"
+//						   +"<td>"+obj.data[i].remark+"</td>"
+//						   +"<td>"+obj.data[i].addtime+"</td>"
+//						   +"<td class='center'><i class='delete icon-pencil repeal'>&nbsp;&nbsp;撤销</i>	</td>"
+//					  "</tr>"
+//				
+//				
+//			}
+//			
+//			Table.fnClearTable();
+//			Table.fnDestroy();
+//			$(".detailTable").html(str);
+//			$(".data-table").attr("style","width: 100%;")
+//		    Table=$('.data-table').dataTable({
+//			    "bDestroy":true,
+//				"bJQueryUI": true,
+//				"retrieve":true,
+//				"sPaginationType": "full_numbers",
+//				"sDom": '<""l>t<"F"fp>',
+//				"bFilter":true,
+//				"bLengthChange":true,
+//				"aaSorting":[[0,"desc"]],
+//				"bAutoWidth":true
+//			});
+//			
+//
+//			
+//			
+//		}
+//	})
 })
 
 //点击明细记录里面的撤销
