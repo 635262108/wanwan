089b7916e1876b9fc308b8bd93417c6d9b304209
sqz2244963 <63526108@qq.com>
2018-02-23 11:04:22 +0800

    新增后台商品模块

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -1,11 +1,62 @@
 <?php
-namespace app\home\controller;
+namespace app\admin\controller;
 use think\Controller;
 
 class Goods extends Base
 {
-    public function index()
-    {	
-    	
+    private $goods;
+
+    public function _initialize(){
+        $this->goods = model('Goods');
+    }
+
+    public function index(){
+        $data = $this->goods->select();
+        return $this->fetch('',[
+            'data'=>$data
+        ]);
+    }
+
+    //显示添加商品界面
+    public function dis_add_goods(){
+        return $this->fetch();
+    }
+
+    //显示更新商品界面
+    public function dis_save_goods($gid){
+        $data = $this->goods->find($gid);
+        if (empty($data)){
+            $this->error('商品信息异常');
+        }
+        return $this->fetch();
+    }
+
+    //更新商品
+    public function save_goods(){
+        $data = input('post.');
+
+        //商品图
+        $img = request()->file('img');
+        if(!empty($img)){
+            $a_img = $img->move(ROOT_PATH . 'public' . DS . 'uploads'. DS .'admin_img');
+            if($a_img){
+                $data['img'] = $a_img->getSaveName();
+            }else{
+                // 上传失败获取错误信息
+                $this->error($a_img->getError());
+            }
+        }
+
+        if(!empty($data['id'])){
+            $res = $this->goods->allowField(true)->save($data,['id'=>$data['id']]);
+        }else{
+            $res = $this->goods->insert($data);
+        }
+
+        if($res){
+            return_info(200,'success');
+        }else{
+            return_info(-1,'fail');
+        }
     }
 }
