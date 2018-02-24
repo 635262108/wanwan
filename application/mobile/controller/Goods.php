a82491b9e7a32fcbc006c5035f043885021b672c
sqz2244963 <63526108@qq.com>
2018-02-24 11:29:23 +0800

    添加商品详情

--- application/mobile/controller/Goods.php
+++ application/mobile/controller/Goods.php
@@ -5,8 +5,22 @@ use think\Controller;
 
 class Goods extends Base
 {
-    public function index()
-    {	
-    	
+    private $goods;
+
+    public function _initialize(){
+        $this->goods = model('Goods');
+    }
+
+    //商品详情
+    public function goods_detail($id){
+    	$result = $this->goods->find($id);
+    	return $this->fetch('',[
+    	    'result'=>$result
+        ]);
+    }
+
+    //选择支付方式
+    public function goods_select_pay(){
+
     }
 }
