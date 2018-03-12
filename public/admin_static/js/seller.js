727f505a848d27fa1e1ff4787b8dc7ec91c2cce4
yangsa1995 <18437976116@163.com>
2018-03-12 18:09:32 +0800

    新系统修改

--- public/admin_static/js/seller.js
+++ public/admin_static/js/seller.js
@@ -1,15 +1,29 @@
 //点击侧边栏的时候
 $(".submenu>a").on("click",function(){
-	$(this).siblings("ul").slideToggle();
+	$(this).siblings("ul").slideToggle(50);
 	$(this).parent(".submenu").siblings(".submenu").find("ul").hide();
 })
 
 
 $(".submenu").on("click",function(){
+	
 	var index=$(this).index();
-	$("#index").removeClass("active");
-	$(this).children(".first_li").addClass("active")
-	$(this).siblings().children(".first_li").removeClass("active");
+	var href=$(this).find("ul li:nth-child(1)").children("a").attr("href");
+	
+	if(index==0){
+		window.location.href=href;
+	}
+	else{
+		
+		$(this).find("ul li:nth-child(1)").children(".line").show();
+		$(this).children(".first_li").addClass("active")
+		$(this).siblings().children(".first_li").removeClass("active");
+		JSON.stringify(localStorage.setItem('Uindex',index));
+		$("iframe").attr("src",href);
+		$("iframe").show();
+		$(".wholePage").children("#content").hide()
+	}
+	
 })
 
 //侧边栏下面的部分
@@ -21,9 +35,6 @@ $(".submenu ul li").on("click",function(){
 	$("iframe").show();
 	$(".submenu a[href="+href_name+"]").siblings(".line").show();
 	$(".submenu a[href="+href_name+"]").parent().siblings("li").find(".line").hide();
-	
 	$(".submenu a[href="+href_name+"]").parents(".submenu").siblings().find(".line").hide();
 	$(".submenu a[href="+href_name+"]").parents(".wholePage").children("#content").hide();
-	
-	
 })
